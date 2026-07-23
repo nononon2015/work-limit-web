3806dacea5b4c47f0e0eceed0c56f70f68809c8b
nononon2015 <nononon2015@users.noreply.github.com>
2026-07-23 23:00:08 +0800

    Raise finish button above footer

--- daily/sw.js
+++ daily/sw.js
@@ -1,5 +1,5 @@
-const CACHE = "stopwork-daily-v9";
-const ASSETS = ["./", "./index.html", "./style.css?v=5", "./app.js?v=9", "./manifest.webmanifest", "./icon.svg", "./og.png"];
+const CACHE = "stopwork-daily-v10";
+const ASSETS = ["./", "./index.html", "./style.css?v=6", "./app.js?v=9", "./manifest.webmanifest", "./icon.svg", "./og.png"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
